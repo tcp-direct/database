e3f2117e323ccb02f865e5645121817496919c26
kayos@tcp.direct <kayos@tcp.direct>
2024-06-08 18:30:09 -0700

    Revert "Fix: remove premature pogreb add"
    This reverts commit 509f844f491d95efd1785e99f4878bffa92726d5.

---
+++ pogreb/keeper_test.go
@@ -0,0 +1,30 @@
+package pogreb
+
+import (
+	"testing"
+
+	"git.tcp.direct/tcp.direct/database"
+)
+
+func Test_Interfaces(t *testing.T) {
+	v := OpenDB(t.TempDir())
+	var keeper interface{} = v
+	if _, ok := keeper.(database.Keeper); !ok {
+		t.Error("Keeper interface not implemented")
+	} else {
+		t.Log("Keeper interface implemented")
+	}
+	vs := v.WithNew("test")
+	var searcher interface{} = vs
+	if _, ok := searcher.(database.Searcher); !ok {
+		t.Error("Searcher interface not implemented")
+	} else {
+		t.Log("Searcher interface implemented")
+	}
+	var filer interface{} = vs
+	if _, ok := filer.(database.Filer); !ok {
+		t.Error("Filer interface not implemented")
+	} else {
+		t.Log("Filer interface implemented")
+	}
+}
